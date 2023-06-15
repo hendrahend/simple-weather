1efd142d8d1e0d6df15a5f99fccd151a490f2e4c
hendra-hendriana <hendh3565@gmail.com>
2023-06-16 00:20:00 +0700

    Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,6 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    fontFamily: {
-      primary: "Poppins",
-    },
     extend: {
       backgroundImage: {
         gradientBg: "url('/src/assets/img/cloud.jpg')",
